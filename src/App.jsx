810844d70c8b5ed92fb2cc5110817f819c8df80f
jezi89 <13301753+jezi89@users.noreply.github.com>
2025-02-20 19:52:36 +0100

    knoppen gemaakt

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Button from "./components/Button.jsx";
 
 function App() {
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,24 @@
-function Button () {
-    const buttonCount = [buttonCount, setButtonCount]
-    return (
-if...
+import {useState} from "react";
 
+function Button () {
+    const [count, setCount] = useState(0)
+
+
+    function countdown() {
+        setCount(prevCount => Math.max(0, prevCount - 1));
+    }
+
+    function countup () {
+        setCount(prevCount => prevCount + 1)
+    }
+    return (
+        <>
+            <div className="button-container">
+            <button onClick={countdown}>-</button>
+            <p>{count}</p>
+            <button onClick={countup}>+</button>
+            </div>
+    </>
     )
 }
+export default Button
